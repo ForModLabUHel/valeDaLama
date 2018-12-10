3dbedb42c8b6c3c60100baafe6ae256a650676bb
checcomi <checcomi@gmail.com>
2018-12-10 14:08:31 +0000

    select axis variables

--- shinyExample.r
+++ shinyExample.r
@@ -2,8 +2,8 @@ library(lubridate);library(shiny)
 library(data.table)
 library(ggplot2)
 library(dplyr)
-allData <- fread("/Users/walterludwick/Documents/data_vdl/allData.csv") #path for wl
-#allData <- fread("C:/Users/minunno/Documents/vdlData/processedData/allData.csv") #path for fm
+# allData <- fread("/Users/walterludwick/Documents/data_vdl/allData.csv") #path for wl
+allData <- fread("C:/Users/minunno/Documents/vdlData/processedData/allData.csv") #path for fm
 load("data/consistData.rdata") ##read data for which fp_id and serial number are consistent
 allData <- allData[serial_number %in% consistData] ##select data for which fp_id and serial number are consistent
 allData$dates <- as.POSIXct(allData$dates)
@@ -20,11 +20,15 @@ ui <- fluidPage(
     
     # Sidebar panel for inputs ----
     sidebarPanel(
-      selectInput(inputId = "variable",
-                  label = "Choose variable:",
+      selectInput(inputId = "Xaxis",
+                  label = "Choose variable for x axis:",
                   choices = c("light", "air_temperature_celsius",
-                              "soil_moisture_percent","dSM")),
-    # Input: Selector for choosing timestep ----
+                              "soil_moisture_percent","dSM","dates")),
+      selectInput(inputId = "Yaxis",
+                  label = "Choose variable for y axis:",
+                  choices = c("light", "air_temperature_celsius",
+                              "soil_moisture_percent","dSM","dates")),
+      # Input: Selector for choosing timestep ----
       selectInput(inputId = "timestep",
                   label = "Choose a timestep:",
                   choices = c("15 min","30 min","1 h","3 h","6 h","12 h","1 d")),
@@ -73,32 +77,21 @@ server <- function(input, output) {
     # x    <- allData[id==input$dataset,dates]
     # print(input$variable)
     subData <- allData[dates %between% c(input$startdate, input$enddate)]
-    if(input$variable=="dSM"){
-      vars <- "soil_moisture_percent"
-      subData    <- subData[longName %in% input$dataset,c("longName","dates",vars),with=FALSE]
-      subData[,dates:=cut(subData$dates, breaks=input$timestep)]
-      subData <- subData[,mean(get(vars),na.rm = T),by=list(longName,dates)]
-      subData <- subData %>% group_by(longName) %>% 
-        mutate(delta = order_by(dates, V1 - lag(V1))) #%>%
-        # mutate(delta = ifelse(is.na(delta), 0, delta))
-      setnames(subData,c("longName","dates","soilPercent",input$variable))
-    }else{
-      vars <- input$variable
-      subData    <- subData[longName %in% input$dataset,c("longName","dates",input$variable),with=FALSE]
-      subData[,hourX:=cut(subData$dates, breaks=input$timestep)]
-      subData <- subData[,mean(get(vars),na.rm = T),by=list(longName,hourX)]
-      setnames(subData,c("longName","dates",input$variable))
-    }
-    
+    subData    <- subData[longName %in% input$dataset]
+    subData[,dates:=cut(subData$dates, breaks=input$timestep)]
+    subData <- subData[, lapply(.SD, mean, na.rm=TRUE), by=list(longName,dates), 
+                       .SDcols=c("light","soil_moisture_percent", "air_temperature_celsius") ] 
+    subData <- subData %>% group_by(longName) %>% 
+      mutate(dSM = order_by(dates, soil_moisture_percent - lag(soil_moisture_percent)))
     
     
     subData$longName <- factor(subData$longName)
     subData$dates <- as.Date(subData$dates)
     ggplot(data=subData,
-           aes_string(x = "dates", y = input$variable,group="longName",color="longName", shape="longName")) +
+           aes_string(x = input$Xaxis, y = input$Yaxis,group="longName",color="longName", shape="longName")) +
       scale_shape_manual(values=1:nlevels(subData$longName)) +
-      xlab("date") +
-      ylab(input$variable) +
+      xlab(input$Xaxis) +
+      ylab(input$Yaxis) +
       # scale_x_date(labels = date_format("%m-%Y"))+
       # theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
       # scale_x_discrete(breaks = round(seq(as.Date(input$startdate), as.Date(input$enddate),
@@ -109,7 +102,7 @@ server <- function(input, output) {
         # hist(x, breaks = bins, col = "#75AADB", border = "white",
     #      xlab = "Waiting time to next eruption (in mins)",
     #      main = "Histogram of waiting times")
-    # plot(x,y[[1]],pch=20,ylab =input$variable,xlab="day" ,main=paste0("sensor ID: ",input$dataset))
+    # plot(x,y[[1]],pch=20,ylab =input$axis,xlab="day" ,main=paste0("sensor ID: ",input$dataset))
   })
   
 }
