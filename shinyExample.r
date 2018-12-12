40d965c71f776500477d65d507294e906a70d615
checcomi <checcomi@gmail.com>
2018-12-12 11:22:26 +0000

    download from drop box if the file do not exists locally

--- shinyExample.r
+++ shinyExample.r
@@ -3,15 +3,16 @@ library(data.table)
 library(ggplot2)
 library(dplyr)
 
-# allData <- fread("/Users/walterludwick/Documents/data_vdl/allData.csv") #path for wl
-# allData <- fread("C:/Users/minunno/Documents/vdlData/processedData/allData.csv") #path for fm
+destfile <- "/Users/walterludwick/Documents/data_vdl/allData.csv" #path for wl
+# destfile <- "C:/Users/minunno/Documents/vdlData/processedData/allData.csv" #path for fm
 
-#### read file from DropBox
-dir.create( "dropbox" )
-download.file("https://www.dropbox.com/s/ngaexvxlazshb0j/allData.csv?dl=1", 
-              "./dropbox/allData.csv" )
-allData <- fread( "./dropbox/allData.csv" )
-unlink( "dropbox", recursive = TRUE )
+if(file.exists(destfile)){
+  allData <-  fread(destfile)
+}else{
+  #### read file from DropBox
+  allData <- fread( "https://www.dropbox.com/s/ngaexvxlazshb0j/allData.csv?dl=1")
+}
+
 
 # load("data/consistData.rdata") ##read data for which fp_id and serial number are consistent
 # allData <- allData[serial_number %in% consistData] ##select data for which fp_id and serial number are consistent
